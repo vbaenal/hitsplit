af3126e747aab12dd3c87a725c8b7cf82a9f3623
Víctor Baena <vicbaeluq@gmail.com>
2024-06-09 00:06:51 +0200

    Apply fmt

--- src/run/manager.rs
+++ src/run/manager.rs
@@ -12,58 +12,36 @@ pub fn next_split(app: &mut HitSplit) {
             app.selected_split += 1;
         } else if app.config.next_split_as_reset {
             app.selected_split = 0;
-            let pbs = category
-                .splits
-                .iter()
-                .map(|split| split.pb)
-                .sum::<u16>();
-            let hits = category
-                .splits
-                .iter()
-                .map(|split| split.hits)
-                .sum::<u16>();
-            category
-                .splits
-                .iter_mut()
-                .for_each(|split| {
-                    if pbs > hits {
-                        split.pb = split.hits;
-                    }
-                    split.hits = 0;
-                });
-        
-        } 
+            let pbs = category.splits.iter().map(|split| split.pb).sum::<u16>();
+            let hits = category.splits.iter().map(|split| split.hits).sum::<u16>();
+            category.splits.iter_mut().for_each(|split| {
+                if pbs > hits {
+                    split.pb = split.hits;
+                }
+                split.hits = 0;
+            });
+        }
     }
 }
 
 pub fn sub_hit(app: &mut HitSplit) {
     if let Some(category) = app.loaded_category.as_mut() {
-        let split = category
-        .splits
-        .get_mut(app.selected_split)
-        .unwrap();
+        let split = category.splits.get_mut(app.selected_split).unwrap();
         if split.hits > 0 {
             split.hits -= 1;
         }
-    }   
+    }
 }
 
 pub fn add_hit(app: &mut HitSplit) {
     if let Some(category) = app.loaded_category.as_mut() {
-        category
-        .splits
-        .get_mut(app.selected_split)
-        .unwrap()
-        .hits += 1;
+        category.splits.get_mut(app.selected_split).unwrap().hits += 1;
     }
 }
 
 pub fn reset(app: &mut HitSplit) {
     if let Some(category) = app.loaded_category.as_mut() {
-        category
-        .splits
-        .iter_mut()
-        .for_each(|split| {
+        category.splits.iter_mut().for_each(|split| {
             split.hits = 0;
         });
         app.selected_split = 0;
@@ -72,10 +50,7 @@ pub fn reset(app: &mut HitSplit) {
 
 pub fn set_pb(app: &mut HitSplit) {
     if let Some(category) = app.loaded_category.as_mut() {
-        category
-        .splits
-        .iter_mut()
-        .for_each(|split| {
+        category.splits.iter_mut().for_each(|split| {
             split.pb = split.hits;
         });
     }
