0f1dfc13d95cbf6de4c099791a1c38ce05683715
Víctor Baena <vicbaeluq@gmail.com>
2024-06-09 00:02:56 +0200

    Fixed crash when changing shortcuts when no category is selected

--- src/run/manager.rs
+++ src/run/manager.rs
@@ -7,82 +7,76 @@ pub fn prev_split(app: &mut HitSplit) {
 }
 
 pub fn next_split(app: &mut HitSplit) {
-    if app.loaded_category.as_ref().unwrap().splits.len() > app.selected_split + 1 {
-        app.selected_split += 1;
-    } else if app.config.next_split_as_reset {
-        app.selected_split = 0;
-        let pbs = app
-            .loaded_category
-            .as_ref()
-            .unwrap()
-            .splits
-            .iter()
-            .map(|split| split.pb)
-            .sum::<u16>();
-        let hits = app
-            .loaded_category
-            .as_ref()
-            .unwrap()
-            .splits
-            .iter()
-            .map(|split| split.hits)
-            .sum::<u16>();
-        app.loaded_category
-            .as_mut()
-            .unwrap()
-            .splits
-            .iter_mut()
-            .for_each(|split| {
-                if pbs > hits {
-                    split.pb = split.hits;
-                }
-                split.hits = 0;
-            });
+    if let Some(category) = app.loaded_category.as_mut() {
+        if category.splits.len() > app.selected_split + 1 {
+            app.selected_split += 1;
+        } else if app.config.next_split_as_reset {
+            app.selected_split = 0;
+            let pbs = category
+                .splits
+                .iter()
+                .map(|split| split.pb)
+                .sum::<u16>();
+            let hits = category
+                .splits
+                .iter()
+                .map(|split| split.hits)
+                .sum::<u16>();
+            category
+                .splits
+                .iter_mut()
+                .for_each(|split| {
+                    if pbs > hits {
+                        split.pb = split.hits;
+                    }
+                    split.hits = 0;
+                });
+        
+        } 
     }
 }
 
 pub fn sub_hit(app: &mut HitSplit) {
-    let split = app
-        .loaded_category
-        .as_mut()
-        .unwrap()
+    if let Some(category) = app.loaded_category.as_mut() {
+        let split = category
         .splits
         .get_mut(app.selected_split)
         .unwrap();
-    if split.hits > 0 {
-        split.hits -= 1;
-    }
+        if split.hits > 0 {
+            split.hits -= 1;
+        }
+    }   
 }
 
 pub fn add_hit(app: &mut HitSplit) {
-    app.loaded_category
-        .as_mut()
-        .unwrap()
+    if let Some(category) = app.loaded_category.as_mut() {
+        category
         .splits
         .get_mut(app.selected_split)
         .unwrap()
         .hits += 1;
+    }
 }
 
 pub fn reset(app: &mut HitSplit) {
-    app.loaded_category
-        .as_mut()
-        .unwrap()
+    if let Some(category) = app.loaded_category.as_mut() {
+        category
         .splits
         .iter_mut()
         .for_each(|split| {
             split.hits = 0;
         });
-    app.selected_split = 0;
+        app.selected_split = 0;
+    }
 }
 
 pub fn set_pb(app: &mut HitSplit) {
-    app.loaded_category
-        .as_mut()
-        .unwrap()
+    if let Some(category) = app.loaded_category.as_mut() {
+        category
         .splits
         .iter_mut()
         .for_each(|split| {
             split.pb = split.hits;
         });
+    }
 }
